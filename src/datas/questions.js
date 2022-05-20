4a75c66eeed6b8e5ac9b2dddb4418cad1319072f
babynghe2003 <himinhpho@gmail.com>
2022-05-20 21:33:09 +0700

    done HomePage and TagsPage

---
+++ src/datas/questions.js
@@ -0,0 +1,136 @@
+var questions = [
+    {
+        userID:1,
+        title: 'Cach de yeu nhieu nguoi cung luc',
+        vote: 10,
+        answers: [
+            {userID:2, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:-2},
+            {userID:4, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:5},
+        ],
+        tags: ['sex','couple']
+    },
+    {
+        userID:2,
+        title: 'Ga co truoc hay trung co truoc',
+        vote: 10,
+        answers: [
+            {userID:1, content: 'dddfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'asdfasdfsdfasdfsssdsdfasdfasdfss', vote:-2},
+            {userID:4, content: 'asdfasdfsdfasdfssdffsddsdfasdfasdfss', vote:5},
+        ],
+        tags: ['abc','sex']
+    },
+    {
+        userID:4,
+        title: 'Как правильно писать код в JavaScript?',
+        vote: 10,
+        answers: [
+            {userID:2, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'dfasdfasdfasdfasdfdf', vote:-2},
+            {userID:1, content: 'fsdfasdfsdfasdfsdsdfasdfasdfss', vote:5},
+        ],
+        tags: ['abc','couple']
+
+    },{
+        userID:1,
+        title: 'Cach de yeu nhieu nguoi cung luc',
+        vote: 10,
+        answers: [
+            {userID:2, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:-2},
+            {userID:4, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:5},
+        ],
+        tags: ['sex','couple']
+    },
+    {
+        userID:2,
+        title: 'Ga co truoc hay trung co truoc',
+        vote: 10,
+        answers: [
+            {userID:1, content: 'dddfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'asdfasdfsdfasdfsssdsdfasdfasdfss', vote:-2},
+            {userID:4, content: 'asdfasdfsdfasdfssdffsddsdfasdfasdfss', vote:5},
+        ],
+        tags: ['abc','sex']
+    },
+    {
+        userID:4,
+        title: 'Как правильно писать код в JavaScript?',
+        vote: 10,
+        answers: [
+            {userID:2, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'dfasdfasdfasdfasdfdf', vote:-2},
+            {userID:1, content: 'fsdfasdfsdfasdfsdsdfasdfasdfss', vote:5},
+        ],
+        tags: ['abc','couple']
+
+    },{
+        userID:1,
+        title: 'Cach de yeu nhieu nguoi cung luc',
+        vote: 10,
+        answers: [
+            {userID:2, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:-2},
+            {userID:4, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:5},
+        ],
+        tags: ['sex','couple']
+    },
+    {
+        userID:2,
+        title: 'Ga co truoc hay trung co truoc',
+        vote: 10,
+        answers: [
+            {userID:1, content: 'dddfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'asdfasdfsdfasdfsssdsdfasdfasdfss', vote:-2},
+            {userID:4, content: 'asdfasdfsdfasdfssdffsddsdfasdfasdfss', vote:5},
+        ],
+        tags: ['abc','sex']
+    },
+    {
+        userID:4,
+        title: 'Как правильно писать код в JavaScript?',
+        vote: 10,
+        answers: [
+            {userID:2, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'dfasdfasdfasdfasdfdf', vote:-2},
+            {userID:1, content: 'fsdfasdfsdfasdfsdsdfasdfasdfss', vote:5},
+        ],
+        tags: ['abc','couple']
+
+    },{
+        userID:1,
+        title: 'Cach de yeu nhieu nguoi cung luc',
+        vote: 10,
+        answers: [
+            {userID:2, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:-2},
+            {userID:4, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:5},
+        ],
+        tags: ['sex','couple']
+    },
+    {
+        userID:2,
+        title: 'Ga co truoc hay trung co truoc',
+        vote: 10,
+        answers: [
+            {userID:1, content: 'dddfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'asdfasdfsdfasdfsssdsdfasdfasdfss', vote:-2},
+            {userID:4, content: 'asdfasdfsdfasdfssdffsddsdfasdfasdfss', vote:5},
+        ],
+        tags: ['abc','sex']
+    },
+    {
+        userID:4,
+        title: 'Как правильно писать код в JavaScript?',
+        vote: 10,
+        answers: [
+            {userID:2, content: 'asdfasdfsdfasdfsdsdfasdfasdfss', vote:2},
+            {userID:3, content: 'dfasdfasdfasdfasdfdf', vote:-2},
+            {userID:1, content: 'fsdfasdfsdfasdfsdsdfasdfasdfss', vote:5},
+        ],
+        tags: ['abc','couple']
+
+    }
+]
+export default questions
