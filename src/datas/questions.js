5309c54edbd34d6965e8d6fd2ccba5c467a2bc71
T <nht250220031@gmail.com>
2022-05-28 00:19:05 +0700

    addquestion

--- src/datas/questions.js
+++ src/datas/questions.js
@@ -2,90 +2,188 @@ var questions = [
     {
         id: 1,
         userID: 1,
-        title: 'Nuôi dạy con tuổi dậy thì',
-        description: 'Con trai mình 10 tuổi, coi như ngấp nghé lứa tuổi dậy thì. Mình mở topic này muốn xin kinh nghiệm các mẹ về giai đoạn này: tuổi nào bắt đầu nhổ giò, quá trình lớn thế nào ? khi nào tính tình bắt đầu thay đổi ? Lời khuyên nên cho ăn uống thêm gì, bổ sung vitamin thuốc men ra sao để thúc cao lớn, cân đối giữa giờ học và tập tành thể thao ra sao? Và mọi câu chuyện hay ho về con của các bạn giai đoạn này ? Rất mong các mẹ vào bàn tán rôm rả !',
+        title: 'Who can help me?',
+        description: 'Do I know how to access sexual health services in my local area including services for contraception, abortion and counselling?',
+        vote: 15,
+        answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+        tags: ['health','service']
+    },{
+        id: 2,
+        userID: 1,
+        title: 'Long-term relationship',
+        description: 'What are the challenges of long-term commitments and the qualities needed for successful loving relationships?',
+        vote: 7,
+        answers: [
+            {
+                userID: 2, content: 'Long-term commitments are not always easy. They require a lot of commitment, time, and money. This can be a challenge for some people who do not want to commit to something that they cannot change in the future. However, long-term commitments can also be beneficial in the sense that it is easier to plan ahead and achieve goals.',
+                vote: 5
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: 5
+            },
+        ],
+        tags: ['love']
+    },{
+        id: 3,
+        userID: 1,
+        title: 'lesbian or bisexual to family and friends',
+        description: 'What is the experience like of ‘coming out’ about being gay, lesbian or bisexual to family and friends?',
         vote: 6,
         answers: [
             {
-                userID: 2, content: 'con gái mới cần quan tâm đặc biết chứ con trai thì cứ để tự nhiên, chăm sóc dinh dưỡng cho cháu là được rồi',
+                userID: 2, content: 'There are no universally correct ways to come out and every person will have a different experience.',
                 vote: 5
             },
             {
-                userID: 3, content: 'hihi, con trai dậy thì sau nhưng sức ép chiều cao lại lớn hơn nên mình muốn hỏi kinh nghiệm để tận dụng tốt nhất độ tuổi này cho chiều cao. Các mẹ có con gái cũng vào chia sẻ cho vui nhé. Nhiều thông tin có ích cho cả con trai và con gái mà.',
+                userID: 3, content: "Coming out is the process of revealing one's sexual orientation to others. The term coming out can refer to revealing one’s identity in a social climate that is unaccepting of LGBT people, or it can refer to the process of recognizing one’s own same-sex attractions.",
                 vote: 3
-            },
-            {
-                userID: 4, content: 'Em cũng đang nuôi con nhỏ, vào học hỏi kinh nghiệm các mẹ ạ',
-                vote: 3
-            },
+            },    
         ],
-        tags: ['age','boy']
-    },
-    {
-        id: 2,
-        userID: 2,
-        title: 'Con trai tuổi dậy thì...',
-        description: 'Tôi có 2 con trai đang tuổi dậy thì ,thật khó khăn khi dạy cho chúng biết về giới tính .Xin giúp tôi với!',
-        vote: 10,
-        answers: [
-            {
-                userID: 2, content: 'đây là một lứa tuổi nhạy cảm, nên để ông bố nói ra thì hay hơn, nói thật khéo vào, mà để nói khoé thì phải hết sức để ý từng cái thay đổi nhỏ của cháu mà đặt vấn đề, như thế các cháu sẽ đỡ ngại và rụt rè khi chia sẽ vấn đề với bố',
-                vote: 5
-            },
-            {
-                userID: 3, content: 'Nhà xuất bản Kim đồng mới xuất bản bộ 4 tập truyện tranh : "Thắc mắc của tuổi mới lớn về giới tính" đó. Bạn thử tham khảo xem, biết đâu con bạn cũng thích đọc truyện tranh nhỉ!',
-                vote: 8
-            },
-        ],
-        tags: ['boy']
-    },
-    {
-        id: 3,
-        userID: 2,
-        title: 'Nên Giáo Dục Giới Tính Cho Con Từ Lúc Nào?',
-        description: 'Nên Giáo Dục Giới Tính Cho Con Từ Lúc Nào?',
-        vote: 8,
-        answers: [
-            {
-                userID: 2, content: 'mình nghĩ là khi bé học hết cấp 1',
-                vote: 5
-            },
-            {
-                userID: 3, content: 'Nếu giáo dục giới tính cho con thì mình nghĩ khi con bắt đầu hiểu biết, mình nên chia sẻ thường xuyên với con, để con có thể hiểu được.',
-                vote: 8
-            },
-            {
-                userID: 4, content: 'Bây giờ đúng là mình luôn suy nghĩ không biết giáo dục giới tính như thế nào cho con là đúng',
-                vote: 0
-            },
-        ],
-        tags: ['education','child']
-    },
-    {
+        tags: ['experience','lbgt','sex']
+    },{
         id: 4,
-        userID: 2,
-        title: 'Giáo Dục Giới Tính Cho Trẻ',
-        description: `Xin giới thiệu các phụ huynh 1 số bài viết liên quan giáo dục giới tính cho con trẻ. Tại sao cần giáo dục giới tính từ nhỏ, như thế nào,... đều có trong bài viết.
-        https://www.facebook.com/notes/tran...y-education-jayneen-sanders/10153962900055939
-        https://www.facebook.com/tranthikimvan/posts/10206693796172694
-        https://www.facebook.com/tranthikimvan/posts/10207448064548932?pnref=story
-        
-        Thân.
-        Bs Trần Thị Kim Vân.`,
-        vote: 32,
+        userID: 1,
+        title: 'What are realistic and unrealistic standards for bodily appearance?',
+        description: 'What are realistic and unrealistic standards for bodily appearance?',
+        vote: 2,
         answers: [
             {
-                userID: 2, content: 'cảm ơn ạ, tài liệu rất bổ ích',
+                userID: 2, content: 'You can find your nearest sexual health services',
                 vote: 5
             },
             {
-                userID: 3, content: 'cảm ơn bác sĩ',
-                vote: 8
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -5
             },
         ],
-        tags: ['sex','education','document']
+        tags: ['appearance']
+    },{
+        id: 5,
+        userID: 1,
+        title: 'What does transgender mean and how can I challenge transphobia?',
+        description: 'What does transgender mean and how can I challenge transphobia?',
+        vote: 4,
+        answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -5
+            },
+        ],
+        tags: ['appearance','service']
+    },{
+        id: 6,
+        userID: 1,
+        title: 'protecting against discrimination',
+        description: 'What protection does the law offer in protecting against discrimination on the grounds of gender and sexual orientation?',
+        vote: 6,
+        answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -5
+            },
+        ],
+        tags: ['health','love','sex']
+    },{
+        id: 7,
+        userID: 1,
+        title: 'Should we have sex and cohabitation before marriage? Why? Why not?',
+        description: 'Should we have sex and cohabitation before marriage? Why? Why not?',
+        vote: 6,
+        answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -5
+            },
+        ],
+        tags: ['sex','marriage']
+    },{
+        id: 8,
+        userID: 1,
+        title: 'difficult to talk about in intimate relationship',
+        description: 'What issues can be difficult to talk about in intimate relationships for example sexual pleasure and contraception and how can this be addressed?',
+        vote: 6,
+        answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -5
+            },
+        ],
+        tags: ['health','service']
     },
-
 
 ]
 export default questions
