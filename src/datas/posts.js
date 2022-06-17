942c8389328fb1b778d5420f2c7f632faf13999f
babynghe2003 <himinhpho@gmail.com>
2022-06-18 04:29:39 +0700

    add post

--- src/datas/posts.js
+++ src/datas/posts.js
@@ -53,58 +53,6 @@ var posts = [
         ],
     },
     {
-        id: 3,
-        userID: 1,
-        title: 'Bài học: Quấy rối, xâm hại và bạo lực tình dục',
-        content: postTemp,
-        createdAt: new Date('2017-07-23T03:24:00'),
-        imgSrc: "https://giaoducgioitinh.online/wp-content/uploads/wordpress-popular-posts/921-featured-70x70.jpg"
-        ,answers: [
-            {
-                userID: 2, content: 'You can find your nearest sexual health services',
-                vote: 5
-            },
-            {
-                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
-                vote: 3
-            },            
-            {
-                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
-                vote: 1
-            },
-            {
-                userID: 4, content: 'You can find answers on the internet',
-                vote: -8
-            },
-        ],
-    },
-    {
-        id: 4,
-        userID: 1,
-        title: 'Bài học: Sức khỏe sinh sản là gì?',
-        content: postTemp,
-        createdAt: new Date('2019-02-23T03:24:00'),
-        imgSrc: "https://giaoducgioitinh.online/wp-content/uploads/wordpress-popular-posts/859-featured-70x70.jpg"
-        ,answers: [
-            {
-                userID: 2, content: 'You can find your nearest sexual health services',
-                vote: 5
-            },
-            {
-                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
-                vote: 3
-            },            
-            {
-                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
-                vote: 1
-            },
-            {
-                userID: 4, content: 'You can find answers on the internet',
-                vote: -8
-            },
-        ],
-    },
-    {
         id: 5,
         userID: 1,
         title: 'Why it’s vital that RSE responds to the lived experiences and realities of young people',
@@ -182,6 +130,283 @@ var posts = [
             },
         ],
     },
+    {
+        id: 8,
+        userID: 1,
+        title: 'Sex Ed in Schools: The Arguments For And Against?',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://blog.4tests.com/wp-content/uploads/2015/12/Screen-Shot-2015-12-30-at-11.30.43-PM.png"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    },
+    {
+        id: 9,
+        userID: 1,
+        title: 'Support for Comprehensive Sexuality Education ',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://i.ytimg.com/vi/uC9UIiNfzTo/maxresdefault.jpg"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    },{
+        id: 10,
+        userID: 1,
+        title: 'The Different Approaches to Sexuality Education',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://images.routledge.com/common/jackets/crclarge/978113880/9781138800700.jpg"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    },{
+        id: 11,
+        userID: 1,
+        title: 'History of sexuality education in the United States',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://images.squarespace-cdn.com/content/v1/55a550a8e4b0c83122fc74f3/1623183089746-M68721B6OJXY0AOUR21L/The+History+of+Sexuality+Education+in+the+United+States.png?format=1500w"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    },{
+        id: 12,
+        userID: 1,
+        title: 'I heard that teaching students about sex only encourages them to have sex. Is this true?',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://images.squarespace-cdn.com/content/v1/55a550a8e4b0c83122fc74f3/1623183089746-M68721B6OJXY0AOUR21L/The+History+of+Sexuality+Education+in+the+United+States.png?format=1500w"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    },{
+        id: 13,
+        userID: 1,
+        title: 'Sex Education in District School Curricular?',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://gdhr.wa.gov.au/documents/10184/94278/RSE+framework+2+-+small.jpg/6284944d-aca5-4f76-b782-f7bf11df0262?t=1414549770410"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    },{
+        id: 14,
+        userID: 1,
+        title: 'Comprehensive Sex Education',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://images.squarespace-cdn.com/content/v1/589b7ebd20099e19599743b0/1563392368580-0H7F3BLQBFNJRI5BG7F6/Comprehensive-Sexuality-Education-2.jpg"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    },{
+        id: 15,
+        userID: 1,
+        title: 'Comprehensive Sex Education',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://images.squarespace-cdn.com/content/v1/589b7ebd20099e19599743b0/1563392368580-0H7F3BLQBFNJRI5BG7F6/Comprehensive-Sexuality-Education-2.jpg"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    },{
+        id: 16,
+        userID: 1,
+        title: 'The pros and cons of sex education in school',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://wiki.tino.org/wp-content/uploads/2021/07/word-image-142.png"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    },{
+        id: 17,
+        userID: 1,
+        title: 'A Case for Sex Education in Schools',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://institute.careerguide.com/wp-content/uploads/2020/09/sexual-education-in-schools.jpg"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    },{
+        id: 18,
+        userID: 1,
+        title: 'Inclusivity in Sex Education',
+        content: postTemp,
+        createdAt: new Date('2022-02-23T03:24:00'),
+        imgSrc: "https://i0.wp.com/asia.gay/wp-content/uploads/2021/01/TeenVogue_SexualIdentity-drippingrainbow.jpg?fit=2560%2C1440&ssl=1"
+        ,answers: [
+            {
+                userID: 2, content: 'You can find your nearest sexual health services',
+                vote: 5
+            },
+            {
+                userID: 3, content: 'You can buy a pregnancy test from pharmacies or some supermarkets, which you can do yourself at home.',
+                vote: 3
+            },            
+            {
+                userID: 4, content: 'You can find answers on the internet, but you need to be very careful about what you search for. You can also find answers on the internet, but you need to be very careful about what you search for.',
+                vote: 1
+            },
+            {
+                userID: 4, content: 'You can find answers on the internet',
+                vote: -8
+            },
+        ],
+    }
 
 ];
 
